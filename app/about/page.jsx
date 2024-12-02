c966639a515240d0563a32a5ad20d4e3f45bb70c
Oscar Garcia <oscaregarcialeon@gmail.com>
2024-12-02 19:28:20 -0400

    Creacion de la pagina de about y galeria.

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,184 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { TbArrowUpRight } from "react-icons/tb";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { desVariants, tagVariants, titleVariants } from "@/utils/animation";
+import { useRef } from "react";
 
 const AboutPage = () => {
-  return (
-    <div>AboutPage</div>
-  )
-}
+  const ref = useRef(null);
 
-export default AboutPage
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end end"]
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+  return (
+    <div>
+      <div
+        className="bg-[url('/image/whoweare.jpg')] 
+      bg-center bg-cover"
+      >
+        <h1
+          className="container py-32 text-6xl font-semibold
+         text-black lg:py-64 text-center tracking-widest"
+        >
+          Who Are We?
+        </h1>
+      </div>
+      <div className="container mx-auto">
+        <div className="pt-4">
+          <motion.h2
+            initial="offscreen"
+            whileInView={"onscreen"}
+            variants={titleVariants}
+            className="text-3xl font-semibold text-center lg:p-10
+          lg:text-5xl"
+          >
+            We have great idea & Interior Design
+          </motion.h2>
+          <motion.p
+            initial="offscreen"
+            whileInView={"onscreen"}
+            variants={desVariants}
+            className="text-2xl text-center font-medium pb-10 mt-5"
+          >
+            Our interior design company is a company that provides interior
+            design services for homes, offices, apartments, and others. We
+            provide the best interior design services for you. We have a team
+            that is experienced in the field of interior.
+          </motion.p>
+        </div>
+        <div className="items-center lg:flex gap-x-8">
+          <motion.div style={{ scale }} ref={ref} className="w-full">
+            <Image src="/image/gallery1123.jpg" width={700} height={700} />
+          </motion.div>
+          {/* Right Content Section */}
+          <motion.div
+            initial="offscreen"
+            whileInView={"onscreen"}
+            variants={tagVariants}
+          >
+            <p className="pb-8 tracking-wide mt-6 mx-auto">
+              We are international Architects. We believe that today it is
+              fundamental to totally rethink archi-tectural education.
+              Confluence not only integrates new visions on society but also
+              incorporates new methods and contemporary tools linked to
+              creativity, production and communication. De-signed and
+              handcrafted to hold and showcase my year two architecture
+              portfolio, the unfolding box allows portfolio sheets. <br />
+              <br />
+              We are international Architects. We believe that today it is
+              fundamental to totally rethink archi-tectural education.
+              Confluence not only integrates new visions on society but also
+              incorporates new methods and contemporary tools linked to
+              creativity, production and communication. De-signed and
+              handcrafted to hold and showcase my year two architecture
+              portfolio, the unfolding box allows portfolio sheets. <br />
+              <br />
+              <span className="text-xl font-extrabold tracking-tight">
+                The backpiperARCH, we share a belief in the transformational
+                power of people united in a common purpose.
+              </span>
+            </p>
+            <Button
+              className="inline-flex items-center px-8 py-3 shadow-lg 
+            hover:bg-gray-800 hover:ring-gray-950 ring-offset-2 "
+            >
+              Read More
+              <TbArrowUpRight className="w-5 h-5 ml-2" />
+            </Button>
+          </motion.div>
+        </div>
+        {/* Team Section */}
+        <div className="lg:py-20 mx-auto">
+          <div className="pt-8 pb-4">
+            <motion.h1
+              initial="offscreen"
+              whileInView={"onscreen"}
+              variants={titleVariants}
+              className="text-4xl font-bold tracking-wider text-center"
+            >
+              Team
+            </motion.h1>
+          </div>
+
+          <div className="grid py-8 gap-20 lg:grid-cols-3">
+            <motion.div
+              initial="offscreen"
+              whileInView={"onscreen"}
+              variants={titleVariants}
+              className="border-2 border-primary"
+            >
+              <div className="p-4 text-center bg-gray-100 -m-0.5 aspect-square dark:bg-tertiary transition hover:-translate-y-3 hover:-translate-x-3">
+                <Image
+                  src="/image/profile2.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full mx-auto"
+                />
+                <h2 className="py-4 text-2xl font-semibold uppercase">
+                  Building Surveys
+                </h2>
+                <p className="text-sm">
+                  Creativity is the obility to generate, create, or discover new
+                  ideas, solutions, and possibilities.
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial="offscreen"
+              whileInView={"onscreen"}
+              variants={desVariants}
+              className="border-2 border-primary"
+            >
+              <div className="p-4 text-center bg-gray-100 -m-0.5 aspect-square dark:bg-tertiary transition hover:-translate-y-3 hover:-translate-x-3">
+                <Image
+                  src="/image/profile3.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full mx-auto"
+                />
+                <h2 className="py-4 text-2xl font-semibold uppercase">
+                  Building Surveys
+                </h2>
+                <p className="text-sm">
+                  Creativity is the obility to generate, create, or discover new
+                  ideas, solutions, and possibilities.
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              initial="offscreen"
+              whileInView={"onscreen"}
+              variants={tagVariants}
+              className="border-2 border-primary"
+            >
+              <div className="p-4 text-center bg-gray-100 -m-0.5 aspect-square dark:bg-tertiary transition hover:-translate-y-3 hover:-translate-x-3">
+                <Image
+                  src="/image/profile1.jpg"
+                  width={200}
+                  height={200}
+                  className="rounded-full mx-auto"
+                />
+                <h2 className="py-4 text-2xl font-semibold uppercase">
+                  Building Surveys
+                </h2>
+                <p className="text-sm">
+                  Creativity is the obility to generate, create, or discover new
+                  ideas, solutions, and possibilities.
+                </p>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const GalleryPage = () => {
   return (
-    <div>GalleryPage</div>
+    <div className='container mx-auto py-10 grid grid-cols-2 md:grid-cols-4 gap-4'>
+      <div className='flex flex-col gap-4'>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/badroom.jpg" alt="gallery0" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery1.jpg" alt="gallery1" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery2.jpg" alt="gallery2" />
+        </div>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/swiper1.jpg" alt="swiper1" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery11.jpg" alt="gallery11" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/living.jpg" alt="living" />
+        </div>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery14.jpg" alt="gallery14" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery15.jpg" alt="gallery15" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery12.jpg" alt="gallery12" />
+        </div>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery7.jpg" alt="gallery7" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery11.jpg" alt="gallery11" />
+        </div>
+        <div>
+          <img className='h-auto max-w-full rounded-lg hover:scale-90 transition-all duration-200' src="/image/gallery3.jpg" alt="gallery3" />
+        </div>
+      </div>
+    </div>
   )
 }
 
